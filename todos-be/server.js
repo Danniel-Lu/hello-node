5b28044ef2577f0cd6f2610b5860b628d4f1a323
danlu <dandesign0818@gmail.com>
2021-10-30 14:30:15 +0800

    連線資料庫

--- todos-be/server.js
+++ todos-be/server.js
@@ -1,14 +1,27 @@
 const express = require("express");
+const mysql = require("mysql");
+const Promise = require("bluebird");
+require("dotenv").config();
+
+let connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_POST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
+});
+
+// 利用 bluebird 把 connection 的函式都變成 promise
+connection = Promise.promisifyAll(connection);
 
 let app = express();
 
-app.get("/", (req, res) => {
-  res.send("我是首頁");
-});
-app.get("member", (req, res) => {
-  res.send("我是會員頁");
+app.get("/api/todos", async (req, res) => {
+  let data = await connection.queryAsync("SELECT * FROM todos");
+  res.json(data);
 });
 
 app.listen(3001, () => {
+  connection.connect();
   console.log("express app 啟動了喔");
 });
